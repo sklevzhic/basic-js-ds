1e9d61aa3b938de8dbc5735e264e74075738395b
sklevzhic <sklevzhic@gmail.com>
2022-10-16 12:14:56 +0300

    fix

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,80 +1,131 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
-const { Node } = require('../extensions/list-tree.js');
+const {Node} = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
 
-  constructor() {
-    this.rootNode = null
-  }
-
-  root() {
-    return this.rootNode
-  }
-
-  add(data) {
-    let newNode = new Node(data)
-    if (!this.rootNode) {
-      this.rootNode = newNode
-      return
+    constructor() {
+        this.rootNode = null
     }
 
-    let currentValue = this.rootNode
+    root() {
+        return this.rootNode
+    }
 
-    while (currentValue) {
-      if (currentValue.data > newNode.data) {
-        if (!currentValue.left) {
-          currentValue.left = newNode
-          return;
+    add(data) {
+        let newNode = new Node(data)
+        if (!this.rootNode) {
+            this.rootNode = newNode
+            return
         }
-        currentValue = currentValue.left
-      } else {
-        if (!currentValue.right) {
-          currentValue.right = newNode
-          return;
+
+        let currentValue = this.rootNode
+
+        while (currentValue) {
+            if (currentValue.data > newNode.data) {
+                if (!currentValue.left) {
+                    currentValue.left = newNode
+                    return;
+                }
+                currentValue = currentValue.left
+            } else {
+                if (!currentValue.right) {
+                    currentValue.right = newNode
+                    return;
+                }
+                currentValue = currentValue.right
+            }
         }
-        currentValue = currentValue.right
-      }
+
     }
 
-  }
-
-  has(data) {
-    return !!this.find(data)
-  }
-
-  find(data) {
-    let recursiveSearch = (node, el) => {
-      if (data === node.data) {
-        return node
-      }
-      if (node.data > data) {
-        return (node.left) ? recursiveSearch(node.left, el) : null
-      }
-      if (node.data < data) {
-        return (node.right) ? recursiveSearch(node.right, el) : null
-      }
+    has(data) {
+        let res = false
+        this.traverseDFS(node => {
+            if (node.data === data) {
+                res = true
+            }
+        })
+        return res
     }
-    return recursiveSearch(this.rootNode, data)
-  }
 
-  remove(data) {
+    find(data) {
+        let res = null
+        this.traverseDFS(node => {
+            if (node.data === data) {
+                res = node
+            }
+        })
+        return res
+    }
 
-  }
+    remove(data) {
+        let parentNode = null
+        let leftNode = null
+        let rightNode = null
 
-  min() {
-  }
+        this.traverseDFS(node => {
+            if (node.left !== null || !!node.right !== null) {
+                if ((node.left?.data === data) || (node.right?.data === data)) {
+                    parentNode = node
+                    leftNode = node.left
+                    rightNode = node.right
+                }
+            }
 
-  max() {
-  }
+        })
+tree.add(4)
+    }
+
+    min() {
+        let res = this.rootNode.data
+        this.traverseDFS(node => {
+            if (node.data < res) {
+                res = node.data
+            }
+        })
+        return res
+    }
+
+    max() {
+        let res = this.rootNode.data
+        this.traverseDFS(node => {
+            if (node.data > res) {
+                res = node.data
+            }
+        })
+        return res
+    }
+
+    preOrder(node, callback) {
+        if (!node) return;
+        if (callback) callback(node)
+        this.preOrder(node.left, callback)
+        this.preOrder(node.right, callback)
+    }
+
+    traverseDFS(callback) {
+        return this.preOrder(this.rootNode, callback)
+    }
 }
 
+let tree = new BinarySearchTree()
+
+tree.add(3)
+tree.add(4)
+tree.add(5)
+tree.add(1)
+tree.add(2)
+tree.add(0)
+tree.add(15)
+tree.remove(5)
+console.log(tree)
 
 
 module.exports = {
-  BinarySearchTree
+    BinarySearchTree
 };
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,55 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
-/**
- * Given a singly linked list of integers l and an integer k,
- * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  // let firstNode = initial;
-  // // let prev =
-  // let i = 0
-  // do {
-  //   if (initial.value !== k) {
-  //     if (i === 0) {
-  //       firstNode = initial
-  //       i++
-  //     } else {
-  //       if (initial.next) {
-  //         firstNode = initial.next
-  //       }
-  //     }
-  //   } else {
-  //     // console.log("else", initial)
-  //     // if (i === 0) {
-  //     //
-  //     // } else {
-  //     //
-  //     // }
-  //   }
-  //   initial = initial.next
-  // } while (initial)
-  // console.log(firstNode)
-  // return firstNode
+
+function removeKFromList(initial, k) {
+  let firstNode;
+
+  while (initial) {
+      if (initial.value !== k) {
+        firstNode = initial
+        break
+      }
+      initial = initial.next
+  }
+
+  let currentNode = firstNode
+  while (currentNode.next) {
+    if (currentNode.next.value === k) {
+      currentNode.next = currentNode.next.next
+      continue
+    }
+    currentNode = currentNode.next
+  }
+  return firstNode
 }
 
 module.exports = {
-  removeKFromList
+    removeKFromList
 };
